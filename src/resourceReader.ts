d3deb98df62f1b8c074a8b834f4022c55f6fb16e
Jamie Taylor <Jamiet-11@hotmail.co.uk>
2021-12-01 22:51:59 +0000

    Removed console log

--- src/resourceReader.ts
+++ src/resourceReader.ts
@@ -1,7 +1,6 @@
 import { readFileSync } from 'fs';
 
 export const getTextForDay = (day: string): string[] => {
-    console.log(__dirname)
   const file = readFileSync(`${__dirname}\\resources\\day-${day}.txt`, 'utf-8');
   return file.split('\n');
 };
