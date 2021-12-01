22d39769144ec341018c373d88790d0b75affd26
Jamie Taylor <Jamie.Taylor@ao.com>
2021-12-01 23:42:41 +0000

    Minor refactor of day one to become slightly more readable

--- src/days/one.test.ts
+++ src/days/one.test.ts
@@ -1,16 +1,19 @@
 import { getTextForDay } from '../resourceReader';
 
-const depthArray = getTextForDay('one').map(Number);
+const measurements = getTextForDay('one').map(Number);
 
 describe('Day one sonar sweep', (): void => {
-  it('Puzzle one - count number of times the depth increases', (): void => {
-    const depthIncreaseCount = depthArray.filter((depth, i) => {
-      return depthArray[i - 1] < depth ? 1 : 0;
+  it('Puzzle one - count number of times the currentDepth increases', (): void => {
+    const totalDepthIncrements = measurements.filter((currentDepth, i) => {
+      const previousDepth = measurements[i - 1];
+      const depthHasIncreased = previousDepth < currentDepth;
+      return depthHasIncreased;
     }).length;
 
-    expect(depthIncreaseCount).toBe(1316);
+    expect(totalDepthIncrements).toBe(1316);
   });
 
+<<<<<<< HEAD
   it('Puzzle two - three measurement sliding window', (): void => {
     const windowDepthIncreaseCount = depthArray.filter((depth, i) => {
       const nextDepth = depthArray[i + 1];
@@ -21,7 +24,23 @@ describe('Day one sonar sweep', (): void => {
       const windowB = nextDepth + twoDepthsLater + threeDepthsLater;
       return windowA < windowB ? 1 : 0;
     }).length;
+=======
+  it('Puzzle two -three measurement sliding window', (): void => {
+    const totalDepthIncrements = measurements.filter(
+      (currentDepth, i) => {
+        const nextDepth = measurements[i + 1];
+        const twoDepthsLater = measurements[i + 2];
+        const threeDepthsLater = measurements[i + 3];
 
-    expect(windowDepthIncreaseCount).toBe(1344);
+        const currentSlidingWindow = currentDepth + nextDepth + twoDepthsLater;
+        const nextSlidingWindow = nextDepth + twoDepthsLater + threeDepthsLater;
+
+        const depthHasIncreased = currentSlidingWindow < nextSlidingWindow;
+        return depthHasIncreased;
+      }
+    ).length;
+>>>>>>> e39f7eb (Minor refactor of day one to become slightly more readable)
+
+    expect(totalDepthIncrements).toBe(1344);
   });
 });
