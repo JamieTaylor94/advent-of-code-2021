f100b090b9cb77a5c2989924655debcb8c93ccdd
Jamie Taylor <Jamie.Taylor@ao.com>
2021-12-02 16:31:49 +0000

    Minor refactor

--- src/days/two.test.ts
+++ src/days/two.test.ts
@@ -2,27 +2,15 @@ import { getTextForDay } from '../resourceReader';
 
 const course = getTextForDay('two');
 
-interface Command {
-  direction: string;
-  placesToMove: number;
-}
-
-const commands = course.map((command) => {
-  const parts = command.split(' ');
-  return {
-    direction: parts[0],
-    placesToMove: parseInt(parts[1])
-  } as Command;
-});
-
 describe('Day two - Dive!', (): void => {
   it('Part One ', (): void => {
     let horizontalPostion = 0;
     let depth = 0;
 
-    commands.forEach((c) => {
-      const direction = c.direction;
-      const placesToMove = c.placesToMove;
+    course.forEach((line) => {
+      const command = parseLine(line);
+      const { direction, placesToMove } = command;
+
       if (direction === 'down') {
         depth += placesToMove;
       } else if (direction === 'up') {
@@ -40,9 +28,9 @@ describe('Day two - Dive!', (): void => {
     let aim = 0;
     let depth = 0;
 
-    commands.forEach((c) => {
-      const direction = c.direction;
-      const placesToMove = c.placesToMove;
+    course.forEach((line) => {
+      const command = parseLine(line);
+      const { direction, placesToMove } = command;
 
       if (direction === 'down') {
         aim += placesToMove;
@@ -57,3 +45,17 @@ describe('Day two - Dive!', (): void => {
     expect(horizontalPostion * depth).toBe(2006917119);
   });
 });
+
+const parseLine = (line: string) => {
+  const parts = line.split(' ');
+
+  return {
+    direction: parts[0],
+    placesToMove: parseInt(parts[1])
+  } as Command;
+};
+
+interface Command {
+  direction: string;
+  placesToMove: number;
+}
